fe12e00f9e3f2c3f2fb4aa45eb576ada94792907
Oleg Mind <oleg22597@mail.ru>
2022-07-24 09:24:29 +0300

    fix images

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md-body">
     <div class="card__wrapper" v-for="item in items" :key="item.id">
-      <Card :title="item.title" :name="`${item.lvl} lvl`">
+      <Card :title="item.title" :name="`${item.lvl} lvl`" :imgUrl="item.img">
         {{ item.description }}
       </Card>
     </div>
@@ -10,26 +10,14 @@
 
 <script>
 import Card from '@/components/UI/Card.vue'
+import items from '@/seeders/items.js'
 export default {
   components: {
     Card
   },
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          title: 'archer',
-          description: 'archer archer',
-          lvl: 4
-        },
-        {
-          id: 2,
-          title: 'warrior',
-          description: 'warrior warrior',
-          lvl: 5
-        }
-      ]
+      items: items
     }
   }
 }
---
+++ src/seeders/items.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'archer',
+    description: 'archer archer',
+    img: require('@/assets/img/archer.png'),
+    lvl: 4
+  },
+  {
+    id: 2,
+    title: 'warrior',
+    description: 'warrior warrior',
+    img: require('@/assets/img/wizard.png'),
+    lvl: 5
+  },
+  {
+    id: 3,
+    title: 'goblin',
+    description: 'goblin goblin',
+    img: require('@/assets/img/goblin.png'),
+    lvl: 4
+  }
+]
